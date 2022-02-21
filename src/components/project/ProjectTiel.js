e02859a6761967c2bb5e9ee09894401681335e75
AmitGupta7580 <amitgupta758000@gmail.com>
2022-02-21 13:18:58 +0530

    Update Styling of Projects Page

--- src/components/project/ProjectTiel.js
+++ src/components/project/ProjectTiel.js
@@ -20,18 +20,20 @@ class ProjectTiel extends Component {
                 <figure>
                     <img src={project.IMAGE} alt={project.NAME} />
                     <figcaption>
-                        {project.VIDEO !== "" && <a href={project.VIDEO} id="projects-tiel-video">
-                            <i className="fas fa-video"></i>
-                        </a>}
-                        {project.LINK !== "" && <a href={project.LINK} id="projects-tiel-link">
-                            <i className="fas fa-link"></i>
-                        </a>}
-                        {project.CODE !== "" && <a href={project.CODE} id="projects-tiel-code">
-                            <i className="fab fa-github"></i>
-                        </a>}
+                        <div className="projects-tiel-link">
+                            {project.VIDEO !== "" && <a href={project.VIDEO} >
+                                <i className="fas fa-video"></i>
+                            </a>}
+                            {project.LINK !== "" && <a href={project.LINK} >
+                                <i className="fas fa-link"></i>
+                            </a>}
+                            {project.CODE !== "" && <a href={project.CODE} >
+                                <i className="fab fa-github"></i>
+                            </a>}
+                        </div>
                         <h4>{project.NAME}</h4>
                         <p>{project.DESCRIPTION}</p>
-                        <div id="projects-tiel-tech">
+                        <div className="projects-tiel-tech">
                             {project.TECH.map((tech) => {
                                 return <TechTag tech={tech} ></TechTag>
                             })}
